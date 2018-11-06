ef893d4b4226f2a1833ca92e6575502e160f0030
Fabio Masao Hosono <fabiomasao@hotmail.com>
2018-11-06 18:43:51 -0200

    Alterações 06/11/2018

--- como-fazer/index.js
+++ como-fazer/index.js
@@ -1,20 +1,35 @@
 const express = require('express')
 const app = express()
 const axios = require ('axios')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded())
 
 const port = process.env.PORT || 3000
 
 
 app.get('/', async(request, response) => {
     const content = await axios.get('https://como-fazer-node.firebaseio.com/teste.json')
-    await axios.post('https://como-fazer-node.firebaseio.com/categorias.json', {
-        categoria: 'Receitas' 
-    })
+    
     console.log(content.data)
     response.render('index', {i: content.data})
 })
+
+app.get('/categorias/nova', (req, res) =>{
+    res.render('categorias/nova')
+})
+
+app.post('/categorias/nova', async(req, res)=>{
+    await axios.post('https://como-fazer-node.firebaseio.com/categorias.json', {
+        categoria: req.body.categoria 
+    })
+    
+    res.send(req.body)
+})
+
+
+
 
 app.listen(port, (err) =>{
     if (err){
