611b658df37e76946c75a5b6491fdbc3c4f57b0e
Fabio Masao Hosono <fabiomasao@hotmail.com>
2018-11-08 18:55:37 -0200

    Update como-fazer/index.js

--- como-fazer/index.js
+++ como-fazer/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const categorias = require('.routes/categorias')
+const categorias = require('./routes/categorias')
 
 
 app.set('view engine', 'ejs')
